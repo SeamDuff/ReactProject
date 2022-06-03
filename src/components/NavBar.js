69d434ddd9ed905ac4906eb501e0985a88ba1a07
ManuOreguy <manuoreguy@gmail.com>
2022-06-02 23:21:52 -0300

    Desafio 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return(
+    <header>
+        <h1>Sevuchitas</h1>
+        <nav>
+            <a href="#">Inicio</a>
+            <a href="#">Productos</a>
+            <a href="#">About</a> 
+            <a href="#">Contacto</a>
+        </nav>
+    </header>
+    )
+}
+
+export default NavBar
